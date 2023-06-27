9b1816fa36ce7931c03c7e0db6af30da7b6dc378
Aldo Apicella <71682065+Skiper132@users.noreply.github.com>
2023-06-27 09:57:11 -0500

    Enum Direccion

--- src/com/battleship/modelo/Direccion.java
+++ src/com/battleship/modelo/Direccion.java
@@ -1,5 +1,29 @@
 package com.battleship.modelo;
 
+/**
+ * Representa las direcciones posibles en las que se puede posicionar un barco.
+ * Cada dirección tiene un incremento de fila y columna asociado, que se utiliza para
+ * calcular las coordenadas de las casillas que ocupará el barco.
+ */
 public enum Direccion {
-    NORTE, SUR, ESTE, OESTE
+    NORTE(0, -1),
+    SUR(0, 1),
+    ESTE(1, 0),
+    OESTE(-1, 0);
+
+    private final int desplazamientoFila;
+    private final int desplazamientoColumna;
+
+    Direccion(int desplazamientoFila, int desplazamientoColumna) {
+        this.desplazamientoFila = desplazamientoFila;
+        this.desplazamientoColumna = desplazamientoColumna;
+    }
+
+    public int getDesplazamientoFila() {
+        return desplazamientoFila;
+    }
+
+    public int getDesplazamientoColumna() {
+        return desplazamientoColumna;
+    }
 }
