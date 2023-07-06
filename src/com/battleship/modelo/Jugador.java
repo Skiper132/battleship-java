2a0bc5eb9f1b4ab2ccf6e849cecad4a40f25d3c1
Aldo Apicella <71682065+Skiper132@users.noreply.github.com>
2023-07-06 00:55:08 -0500

    Incorporación de gestión de barcos en la clase Jugador utilizando HashMaps agregué la capacidad para manejar los barcos en la clase Jugador a través de una estructura de datos HashMap.
    Escogí usar un HashMap porque permite una búsqueda rápida y eficiente de los barcos utilizando su nombre como clave. Esta elección proporciona una forma fácil y directa de acceder a cualquier barco asociado a un jugador en tiempo constante O(1). Esto es fundamental para mantener una alta eficiencia a medida que escalamos el número de barcos.
    
    Las claves del HashMap son los nombres de los barcos, y los valores son las instancias del barco. En la función crearBarcos, genero todos los barcos de acuerdo a la enumeración NombreBarco y los almaceno en el HashMap.
    
    Además, creé el método obtenerBarco en la clase Jugador, que permite recuperar un barco a través de su nombre.
    
    Como funciona exactamente el metodo crearBarcos():
    
    Este método se encarga de inicializar y llenar el mapa `barcos` con objetos de tipo `Barco`. Cada barco se identifica por su nombre y tiene un tamaño determinado.
    
    1. Primero, el método recorre todos los valores de la enumeración `NombreBarco` usando el bucle for-each: `for (NombreBarco nombreBarco : NombreBarco.values())`. Aquí, `NombreBarco.values()` devuelve un array que contiene todas las constantes de `NombreBarco` (Lancha, Submarino, Acorazado).
    
       Ejemplo gráfico:
    
       ```
       Lancha, Submarino, Acorazado
       ```
    
    2. Dentro de este bucle, se realiza otro bucle `for` que va desde `0` hasta la cantidad de barcos de ese tipo específico (`nombreBarco.cantidad`).
    
       Ejemplo gráfico para `Lancha` que tiene `cantidad = 2`:
    
       ```
       Iteración 1: i = 0
       Iteración 2: i = 1
       ```
    
    3. Dentro de este segundo bucle, se llama al método estático `NombreBarco.obtenerNombre(nombreBarco.longitud, i+1)`. Este método recibe la longitud del barco y el número de barco y devuelve el nombre completo del barco.
    
       Ejemplo gráfico para la primera iteración de `Lancha`:
    
       ```
       NombreBarco.obtenerNombre(2, 1) --> "Lancha 1"
       ```
    
    4. Una vez obtenido el nombre del barco, se crea una nueva instancia de `Barco` con la longitud específica y el número de barco (`new Barco(nombreBarco.longitud, i+1)`). Este objeto `Barco` se añade al mapa `barcos`, usando el nombre del barco como clave.
    
       Ejemplo gráfico para la primera iteración de `Lancha`:
    
       ```
       barcos.put("Lancha 1", new Barco(2, 1))
       ```
    
    5. Este proceso se repite para todos los tipos de barcos y para todas las cantidades especificadas de cada tipo de barco.
    
    Al final, el mapa `barcos` estará lleno de objetos `Barco`, cada uno con su nombre correspondiente como clave.

--- src/com/battleship/modelo/Jugador.java
+++ src/com/battleship/modelo/Jugador.java
@@ -1,54 +1,53 @@
 package com.battleship.modelo;
 
+import java.util.HashMap;
+
 public class Jugador {
     private String nombre;
-    private Tablero tablero;
-    private Barco[] barcos;
+    private HashMap<String, Barco> barcos;
+
 
     public Jugador(String nombre) {
         this.nombre = nombre;
-        this.tablero = new Tablero();
-        this.barcos = new Barco[4];
-        barcos[0] = new Barco(2, 1);
-        barcos[1] = new Barco(2, 2);
-        barcos[2] = new Barco(3, 1);
-        barcos[3] = new Barco(4, 1);
+        this.barcos = new HashMap<>();
+        crearBarcos();
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public Tablero getTablero() {
-        return tablero;
-    }
-
-    public Barco[] getBarcos() {
-        return barcos;
-    }
-
     /**
-     * Verifica si todos los barcos del jugador están hundidos.
-     *
-     * @return true si todos los barcos están hundidos, false en caso contrario.
+     * Crea los barcos del jugador, funciona con el enum NombreBarco para crear los barcos, utiliza 2 ciclos
+     * anidados para crear la cantidad de barcos especificada en el enum y los agrega al HashMap barcos.
      */
-    public boolean todosHundidos() {
-        for (Barco barco : barcos) {
-            if (barco.getEstado() != EstadoBarco.HUNDIDO) {
-                return false;
+    private void crearBarcos() {
+        for (NombreBarco nombreBarco : NombreBarco.values()) {
+            for (int i = 0; i < nombreBarco.cantidad; i++) {
+                String nombreBarcoCompleto = NombreBarco.obtenerNombre(nombreBarco.longitud, i+1);
+                barcos.put(nombreBarcoCompleto, new Barco(nombreBarco.longitud, i+1));
             }
         }
-        return true;
     }
 
     /**
-     * Verifica si todos los barcos del jugador están posicionados.
+     * Devuelve un barco del jugador según su nombre.
      * 
-     * @return true si todos los barcos están posicionados, false en caso contrario.
+     * @param nombre el nombre del barco.
+     * @return el barco.
      */
-    public boolean todosPosicionados() {
-        for (Barco barco : barcos) {
-            if (barco.getEstado() != EstadoBarco.POSICIONADO) {
+    public Barco obtenerBarco(String nombre) {
+        return barcos.get(nombre);
+    }
+
+    /**
+     * Recorre todos los barcos del jugador y devuelve true si todos están hundidos.
+     * 
+     * @return true si todos los barcos están hundidos, false en caso contrario.
+     */
+    public boolean todosLosBarcosHundidos() {
+        for (Barco barco : barcos.values()) {
+            if (barco.getEstado() != EstadoBarco.HUNDIDO) {
                 return false;
             }
         }
