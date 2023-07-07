b14c3a5bd4e1121729f68542dd27a2e532a6f548
Aldo Apicella <71682065+Skiper132@users.noreply.github.com>
2023-07-07 12:48:42 -0500

    Changes to be committed: 	new file:   src/com/battleship/controlador/ControladorJuego.java 	modified:   src/com/battleship/modelo/Tablero.java 	modified:   src/com/battleship/vista/Pantalla.java  Changes not staged for commit: 	modified:   bin/com/battleship/controlador/Controlador.class 	modified:   bin/com/battleship/modelo/Barco.class 	modified:   bin/com/battleship/modelo/Casilla.class 	modified:   bin/com/battleship/modelo/Direccion.class 	modified:   bin/com/battleship/modelo/EstadoCasilla.class 	modified:   bin/com/battleship/modelo/Jugador.class 	modified:   bin/com/battleship/modelo/Tablero.class 	modified:   bin/com/battleship/vista/Pantalla.class 	modified:   src/com/battleship/modelo/Casilla.java 	modified:   src/com/battleship/modelo/EstadoCasilla.java  Untracked files: 	bin/com/battleship/controlador/ControladorJuego.class 	bin/com/battleship/modelo/NombreBarco.class 	bin/com/battleship/vista/VistaTablero.class

--- src/com/battleship/vista/Pantalla.java
+++ src/com/battleship/vista/Pantalla.java
@@ -1,9 +1,35 @@
 package com.battleship.vista;
 
 import com.battleship.controlador.Controlador;
+import com.battleship.modelo.Casilla;
+import com.battleship.modelo.Tablero;
 public class Pantalla {
     //TODO: Implementar la vista
     public static void main(String[] args) {
         Controlador controlador = new Controlador();
+        
+    }
+
+    public static void mostrarTablero(Tablero tablero) {
+        Casilla[][] casillas = tablero.getCasillas();
+
+        // Imprime el encabezado de las columnas del tablero
+        System.out.print("  ");
+        for (int i = 0; i < casillas[0].length; i++) {
+            System.out.print(i + 1 + " ");
+        }
+        System.out.println();
+
+        // Imprime las filas del tablero
+        for (int i = 0; i < casillas.length; i++) {
+            System.out.print((char) ('A' + i) + " ");
+            for (int j = 0; j < casillas[i].length; j++) {
+                // Ya no necesitamos obtener la coordenada ni buscar la casilla en el tablero
+                Casilla casilla = casillas[i][j];
+                // Obtenemos el símbolo de la casilla y lo imprimimos
+                System.out.print(casilla.getSimbolo() + " ");
+            }
+            System.out.println();
+        }
     }
 }
